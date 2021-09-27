1ea437b98d65becc6981d8d3cefa9b2fd9b326fe
pavel <nechapay@list.ru>
2021-09-27 14:50:59 +0300

    v.2

--- src/Main.java
+++ src/Main.java
@@ -9,26 +9,32 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         int n = 5; // размер массива
-        int[] arr = createArray(n);
-        fillArray(arr, n);
-        printArray(arr, n);
+        int[] arr = createArray();
+        fillArray(arr);
+        printArray(arr);
     }
+
     // создаёт массив размера n
-    public static int[] createArray(int n) {
+    public static int[] createArray() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
         return new int[n];
     }
+
     // заполняет массив arr размера n пользовательским вводом
-    public static void fillArray(int[] arr, int n) {
+    public static void fillArray(int[] arr) {
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.print("Enter array item: ");
             arr[i] = sc.nextInt();
         }
     }
+
     // выводит на печать массив arr размера n
-    public static void printArray(int[] arr, int n) {
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
         }
     }
 }
